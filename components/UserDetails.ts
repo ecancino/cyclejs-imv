65d9eaddff2bab3fb73c74f31db1e356304de996
ecancino <eduardo.cancino@ciudadde.mx>
2019-11-24 22:29:47 -0500

    2 files modified

--- components/UserDetails.ts
+++ components/UserDetails.ts
@@ -1,10 +1,16 @@
-import { dl, dt, dd } from "@cycle/dom";
+import { div, h5, p } from "@cycle/dom";
 
 import A from "./A";
 
-export default function UserDetails({ name, email, website }) {
-  return dl(".user-details", [
-    dt("Name:"),
-    dl([A({ href: `mailto:${email}`, label: name })])
+export default function UserDetails({ id, name, phone, email, address }) {
+  return div(".card", { id }, [
+    div(".card-header", name),
+    div(".card-body", [
+      h5(".card-title", name),
+      p(".card-text", [
+        `${id} ${address.street}, ${address.city}, ${address.zipcode}`
+      ]),
+      A({ style: ".btn.btn-primary", href: `mailto:${email}`, label: email })
+    ])
   ]);
 }
